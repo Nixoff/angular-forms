689b2c04dfd4aa0c61cdb70eb22129d6f33930e3
Gabriel Maciel Teixeira <consultor.131@7comm.com.br>
2022-03-21 17:41:21 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    TemplateFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/template-form/template-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.scss']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: null,
+    email: null
+  }
+
+  onSubmit(form: any) {
+    // console.log(form.controls.email.valid);
+    console.log(this.usuario);
+
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
